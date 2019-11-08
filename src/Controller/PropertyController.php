c13ad70b078910f6019e2781f94e9a5123a6fd9e
Tess07 <developpementnett@gmail.com>
2019-11-08 10:45:38 +0100

    :pagination et filtres

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,14 +2,16 @@
 namespace App\Controller;
 
 use App\Entity\Property;
+use App\Entity\PropertySearch;
+use App\Form\PropertySearchType;
 use App\Repository\PropertyRepository;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Flex\Response as SymfonyResponse;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Flex\Response as SymfonyResponse;
 
 
 class PropertyController extends AbstractController
@@ -35,17 +37,23 @@ class PropertyController extends AbstractController
      * @Route("/biens", name="property.index")
      * @return Response
      */
-    
+
     public function index(PaginatorInterface $paginator, Request $request): Response
-    {           
+    {
+                $search = new PropertySearch();
+                $form = $this->createForm(PropertySearchType::class, $search);
+                $form->handleRequest($request);
+
+
                 $properties = $paginator->paginate(
-                $this->repository->findAllVisibleQuery(),
+                $this->repository->findAllVisibleQuery($search),
                 $request->query->getInt('page', 1),
                 12
             );
             return $this->render('property/index.html.twig', [
                 'current_menu' => 'properties',
-                'properties' => $properties
+                'properties'   => $properties,
+                'form'         => $form->createView()
          ]);
     }
 
@@ -58,17 +66,15 @@ class PropertyController extends AbstractController
     {
         if ($property->getSlug() !== $slug) {
             return $this->redirectToRoute('property.show', [
-              'id' => $property->getId(),
-              'slug' => $property->getSlug()
+              'id'      => $property->getId(),
+              'slug'    => $property->getSlug()
           ], 301);
     }
-      
+
 
         return $this->render('property/show.html.twig', [
-          'property' => $property,
-          'current_menu' => 'properties'
+          'property'        => $property,
+          'current_menu'    => 'properties'
         ]);
     }
 }
-
-
--- src/Repository/PropertyRepository.php
+++ src/Repository/PropertyRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use Doctrine\ORM\Query;
 use App\Entity\Property;
 use Psr\Log\LoggerInterface;
+use App\Entity\PropertySearch;
 use Doctrine\ORM\QueryBuilder;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
@@ -30,10 +31,21 @@ class PropertyRepository extends ServiceEntityRepository
     /**
      * @return Query
      */
-    public function findAllVisibleQuery(): Query
+    public function findAllVisibleQuery(PropertySearch $search): Query
     {
-        return $this->findVisibleQuery()
-            ->getQuery();
+        $query = $this->findVisibleQuery();
+
+        if ($search->getMaxPrice()) {
+            $query = $query
+                ->andWhere('p.price <= :maxprice')
+                ->setParameter('maxprice', $search->getMaxPrice());
+        }
+        if ($search->getMinSurface()) {
+            $query = $query
+                ->andWhere('p.surface >= :minsurface')
+                ->setParameter('minsurface', $search->getMinSurface());
+        }
+        return $query ->getQuery();
     }
 
      /**
